7472913dd5c7b065b5b7c258c367ef063f0eb9ce
MasterMind7777777 <go.go.nest.nest@gmail.com>
2021-09-06 14:10:48 +0300

    Добавлены urls и view-функции

---
+++ yatube/posts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+
+    return render(request, 'posts/index.html')
+
+def group_posts(request):
+
+    return render(request, 'posts/group_posts.html')
