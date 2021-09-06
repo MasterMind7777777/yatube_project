bca39adcf2aa629e3277f9e7edbb1b8c7698c891
MasterMind7777777 <go.go.nest.nest@gmail.com>
2021-09-06 23:08:10 +0300

    set up main page and groups

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+# Получаем модель пользователя.
+User = get_user_model()
+
+
+class Group(models.Model):
+    """Модель сообществ в которые можно 
+       публиковать статьи"""
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    description = models.TextField()
+    
+    def __str__(self):
+        """Значение выводимое при печати"""
+        return self.title
+
+
+class Post(models.Model):
+    """Модель статьи в блоге."""
+    # Текст статьи.
+    text = models.TextField()
+    # Дата публикации.
+    pub_date = models.DateTimeField(auto_now_add=True)
+    # Группа в которой будет опубликована статья(Опциолнално).
+    group = models.ForeignKey(Group, blank=True, null=True, on_delete=models.SET_NULL,)
+    # Автор статьи
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
-# Create your views here.
 def index(request):
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
-    return render(request, 'posts/index.html')
+def group_posts(request, slug):
+    # в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
 
-def group_posts(request):
-
-    return render(request, 'posts/group_posts.html')
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    # Обозначаем шаблон для этого view.
+    template = 'posts/group_list.html'
+    # Набор переменных передоваемый в шаблон.
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
